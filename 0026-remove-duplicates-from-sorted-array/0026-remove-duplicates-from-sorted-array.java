1cb11111ecb009e88b64d46189e8be0c59d735d7
Rachad-4 <136861220+Rachad-4@users.noreply.github.com>
2024-07-29 13:39:50 -0500

    Time: 0 ms (100.00%), Space: 44.4 MB (93.26%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.java
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.java
@@ -1,24 +1,20 @@
 class Solution {
     public int removeDuplicates(int[] nums) {
         if (nums.length < 2) return nums.length;
-        int replacementIndex = 0;
-        int uniqueNumbers = 1;
-
-
-        for (int i = 0; i < nums.length - 1; i++) {
-            if (nums[i] != nums[i+1]){
-                 nums[++replacementIndex] = nums[i + 1];
-            }
-        }
+        
+        int currentNumber = nums[0];
+        int currentNumberIndex = 0;
+        int uniqueNumber = 1;
 
         for (int i = 1; i < nums.length; i++) {
-            if (nums[i] <= nums[i -1]) {
-                break;
+            if (currentNumber != nums[i]) {
+                nums[++currentNumberIndex] = nums[i];
+                currentNumber = nums[i];
+                uniqueNumber++;
             }
-            else
-            uniqueNumbers++;
         }
-
-        return uniqueNumbers;
-    }    
+        
+        return uniqueNumber;
+        
+    }
 }
